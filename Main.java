48f06cf8ae7e60e388134009c9eda2d1432be19a
JermainVonk <djurv@hotmail.com>
2024-09-17 13:53:49 +0200

    heb me huiswerk gemaakt

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+    ApplePieRecipe myApplePie = new ApplePieRecipe();
+        myApplePie.printIngredients(myApplePie.ongezoutenRoomboter);
+        myApplePie.verwarmOven();
+        myApplePie.printIngredients(myApplePie.ei);
+        myApplePie.klopEiLos();
+        myApplePie.printIngredients(myApplePie.witteBastardSuiker);
+        myApplePie.printIngredients(myApplePie.zelfRijzendBakmeel);
+        myApplePie.printIngredients(myApplePie.vanilleSuiker);
+        myApplePie.printIngredients(myApplePie.zout);
+        myApplePie.mengIngredienten();
+        myApplePie.printIngredients(myApplePie.kristalSuiker);
+        myApplePie.printIngredients(myApplePie.kaneel);
+        myApplePie.schilAppels();
+        myApplePie.prepareerSpringvorm();
+        myApplePie.printIngredients(myApplePie.paneerMeel);
+        myApplePie.bedekBodemVorm();
+        myApplePie.printIngredients(myApplePie.zoetZureAppels);
+        myApplePie.printIngredients(myApplePie.kristalSuiker);
+        myApplePie.printIngredients(myApplePie.kaneel);
+        myApplePie.appelsInDeVorm();
+        myApplePie.deegSnijden();
+        myApplePie.printIngredients(myApplePie.ei);
+        myApplePie.strokenLeggen();
+        myApplePie.taartInOven();
+
+    }
+}
